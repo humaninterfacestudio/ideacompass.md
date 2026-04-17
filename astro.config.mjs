91387e89ea113aef91532d001793e92a5f660670
Ömer Balyalı <git@omerbalyali.com>
2026-04-17 14:19:20 +0200

    style: format files
    Signed-off-by: Ömer Balyalı <git@omerbalyali.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,29 +3,29 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false,
-  },
-  site: "https://ideacompass.md",
-  fonts: [
-    {
-      provider: fontProviders.local(),
-      name: "Switzer",
-      cssVariable: "--font-switzer",
-      options: {
-        variants: [
-          {
-            src: ["./src/assets/fonts/Switzer-Variable.woff2"],
-            weight: "normal",
-            style: "normal",
-          },
-          {
-            src: ["./src/assets/fonts/Switzer-VariableItalic.woff2"],
-            weight: "normal",
-            style: "italic",
-          },
-        ],
-      },
-    },
-  ],
+	devToolbar: {
+		enabled: false,
+	},
+	site: "https://ideacompass.md",
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: "Switzer",
+			cssVariable: "--font-switzer",
+			options: {
+				variants: [
+					{
+						src: ["./src/assets/fonts/Switzer-Variable.woff2"],
+						weight: "normal",
+						style: "normal",
+					},
+					{
+						src: ["./src/assets/fonts/Switzer-VariableItalic.woff2"],
+						weight: "normal",
+						style: "italic",
+					},
+				],
+			},
+		},
+	],
 });
